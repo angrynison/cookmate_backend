cbbe52a39101f06201723d5021a3ad6d38e082f6
angry <kalina01255@naver.com>
2026-09-10 18:09:33 +0900

    테스트이름 변경

--- src/test/java/com/cookmate/cookmate/pantry/PantryServiceTest.java
+++ src/test/java/com/cookmate/cookmate/pantry/PantryServiceTest.java
@@ -2,7 +2,9 @@ package com.cookmate.cookmate.pantry;
 
 import com.cookmate.global.type.StorageType;
 import com.cookmate.global.type.Unit;
+import com.cookmate.ingredient.domain.Ingredient;
 import com.cookmate.ingredient.repository.IngredientRepository;
+import com.cookmate.member.domain.Member;
 import com.cookmate.member.repository.MemberRepository;
 import com.cookmate.pantry.domain.Pantry;
 import org.junit.jupiter.api.Assertions;
@@ -17,13 +19,15 @@ import com.cookmate.pantry.dto.PantryRequestDto;
 import com.cookmate.pantry.repository.PantryRepository;
 import com.cookmate.pantry.service.Impl.PantryServiceImpl;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 
-@DisplayName("Pantry 서비스 테스트")
+@DisplayName("Pantry 서비스 unit 테스트")
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 public class PantryServiceTest {
@@ -56,113 +60,89 @@ public class PantryServiceTest {
         //given
         Long member1 = 1L;
         Long member2 = 2L;
-        Long ingredient1 = 1L;
-        Long ingredient2 = 2L;
+        Member mockMember1 = Member.builder().id(member1).build();
+        Member mockMember2 = Member.builder().id(member2).build();
 
-//        Member mockMember1 = Member.builder().id(member1).build();
-//        Member mockMember2 = Member.builder().id(member2).build();
+        given(memberRepository.findById(member1)).willReturn(Optional.of(mockMember1));
+        given(memberRepository.findById(member2)).willReturn(Optional.of(mockMember2));
 
-//        Ingredient mockIngredient1 = Ingredient.builder()
-//                .id(ingredient1)
-//                .defaultExpiry(5)
+        // 재료 등록 요청 생성
+
+        // 유통기한을 적거나, ingredient Id를 넘겨주지 않으면 설정한 IllegalArgumentException이 발생
+//        PantryRequestDto.CreateRequest pantryRequest = PantryRequestDto.CreateRequest.builder()
+//                .name("감자")
+//                .purchaseDate(LocalDate.now())
+//                .storageType(StorageType.냉장)
+//                .quantity(2)
+//                .unit(Unit.EA)
 //                .build();
-//        Ingredient mockIngredient2 = Ingredient.builder()
-//                .id(ingredient2)
-//                .defaultExpiry(3)
-//                .build();
 
-        
-        // h2 db 구현
-        // memberRepository.save() ~~ 구현
-        // ingredientRepository.save() ~~ 구현
+        PantryRequestDto.CreateRequest pantryRequest1 = PantryRequestDto.CreateRequest.builder()
+                .name("감자")
+                .purchaseDate(LocalDate.now())
+                .expiryDate(LocalDate.now().plusDays(5))
+                .storageType(StorageType.냉장)
+                .quantity(2)
+                .unit(Unit.EA)
+                .build();
 
 
-        PantryRequestDto.CreateRequest request1 = new PantryRequestDto.CreateRequest(
-                ingredient1,
-                "감자",
-                LocalDate.now(),
-                null,
-                StorageType.냉장,
-                2,
-                Unit.EA
-        );
-
-        PantryRequestDto.CreateRequest request2 = new PantryRequestDto.CreateRequest(
-                ingredient2,
-                "감자",
-                LocalDate.now(),
-                LocalDate.now().plusDays(3),
-                StorageType.상온,
-                3,
-                Unit.EA
-        );
-
-//        given(memberRepository.findById(member1)).willReturn(Optional.of(mockMember1));
-//        given(memberRepository.findById(member2)).willReturn(Optional.of(mockMember2));
-//
-//        given(ingredientRepository.findById(ingredient1)).willReturn(Optional.of(mockIngredient1));
-//        given(ingredientRepository.findById(ingredient2)).willReturn(Optional.of(mockIngredient2));
+        PantryRequestDto.CreateRequest pantryRequest2 = PantryRequestDto.CreateRequest.builder()
+                .name("당근")
+                .purchaseDate(LocalDate.now())
+                .expiryDate(LocalDate.now().plusDays(3))
+                .storageType(StorageType.상온)
+                .quantity(3)
+                .unit(Unit.EA)
+                .build();
 
         //when
-        pantryService.createPantry(member1, request1);
-        pantryService.createPantry(member2, request2);
+        pantryService.createPantry(member1, pantryRequest1);
+        pantryService.createPantry(member2, pantryRequest2);
 
         //then
         verify(pantryRepository, times(2)).save(any(Pantry.class));
     }
-    
+
     @Test
-    @DisplayName("재료정보 업데이트 테스트")
-    void updatePantry() {
+    @DisplayName("재료정보 업데이트 단위 테스트")
+    void updatePantryTest() {
 
-        //given
+        // Given
         Long memberId = 1L;
-        Long ingredientId = 1L;
+        Long pantryId = 100L;
 
-//        Member mockMember1 = Member.builder().id(memberId).build();
-//        Ingredient mockIngredient1 = Ingredient.builder().id(ingredientId).build();
+        Member mockMember = Member.builder()
+                .id(memberId)
+                .build();
 
-        // h2 db 구현후 테스트 코드 작성 진행
+        Pantry mockPantry = Pantry.builder()
+                .id(pantryId)
+                .member(mockMember)
+                .name("감자")
+                .quantity(2)
+                .expiryDate(LocalDate.now().plusDays(5))
+                .build();
 
-
-        PantryRequestDto.CreateRequest request = new PantryRequestDto.CreateRequest(
-                ingredientId,
-                "감자",
-                LocalDate.now(),
-                LocalDate.now().plusDays(3),
-                StorageType.상온,
-                3,
-                Unit.EA
-                );
-
-//        given(memberRepository.findById(memberId)).willReturn(Optional.of(mockMember1));
-//        given(ingredientRepository.findById(ingredientId)).willReturn(Optional.of(mockIngredient1));
-
-        Long pantryId = pantryService.createPantry(memberId, request);
-
-        Pantry pantry1 = pantryRepository.findById(pantryId)
-                .orElseThrow(() -> new IllegalArgumentException("보유하지 않은 식재료입니다."));
-
-        LocalDate changeExpiry = LocalDate.now().plusDays(5);
+        LocalDate changeExpiry = LocalDate.now().plusDays(10);
         Integer changeQuantity = 5;
 
-        PantryRequestDto.UpdateRequest updateRequest = new PantryRequestDto.UpdateRequest(
-                null,
-                null,
-                changeExpiry,
-                null,
-                changeQuantity
-        );
+        PantryRequestDto.UpdateRequest updateRequest = PantryRequestDto.UpdateRequest.builder()
+                .expiryDate(changeExpiry)
+                .quantity(changeQuantity)
+                .build();
 
-        //when
-        pantryService.updatePantry(pantryId, ingredientId, updateRequest);
+        given(memberRepository.findById(memberId)).willReturn(Optional.of(mockMember));
+        given(pantryRepository.findById(pantryId)).willReturn(Optional.of(mockPantry));
 
-        LocalDate afterExpiry = pantry1.getExpiryDate();
-        Integer afterQuantity = pantry1.getQuantity();
 
-        //then
-        Assertions.assertEquals(changeQuantity, afterQuantity);
-        Assertions.assertEquals(changeExpiry, afterExpiry);
+        // When
+        pantryService.updatePantry(memberId, pantryId, updateRequest);
+
+
+        // Then
+        Assertions.assertEquals(changeQuantity, mockPantry.getQuantity());
+        Assertions.assertEquals(changeExpiry, mockPantry.getExpiryDate());
     }
 
 
